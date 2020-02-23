49f14148e78ebeb61c378c4eace1d25e32447afd
Fran P <fpolig02@gmail.com>
2020-02-23 11:08:19 -0500

    adding code from strava api tutorials

---
+++ Strava_Api/strava_api.js
@@ -0,0 +1,32 @@
+
+const auth_link = "https://www.strava.com/oauth/token"
+
+function getActivites(res){
+
+    const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+    fetch(activities_link)
+        .then((res) => console.log(res.json()))
+}
+
+function reAuthorize(){
+    fetch(auth_link,{
+        method: 'post',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+
+        },
+
+        body: JSON.stringify({
+
+            client_id: 'xxxx',
+            client_secret: 'xxxx',
+            refresh_token: 'xxxx',
+            grant_type: 'refresh_token'
+        })
+    })
+    .then(res => getActivites(res))
+      
+}
+
+reAuthorize()
---
+++ Strava_Api/strava_api.py
@@ -0,0 +1,26 @@
+import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+auth_url = "https://www.strava.com/oauth/token"
+activites_url = "https://www.strava.com/api/v3/athlete/activities"
+
+payload = {
+    'client_id': "xxxx",
+    'client_secret': 'xxxx',
+    'refresh_token': 'xxxx',
+    'grant_type': "refresh_token",
+    'f': 'json'
+}
+
+print("Requesting Token...\n")
+res = requests.post(auth_url, data=payload, verify=False)
+access_token = res.json()['access_token']
+print("Access Token = {}\n".format(access_token))
+
+header = {'Authorization': 'Bearer ' + access_token}
+param = {'per_page': 200, 'page': 1}
+my_dataset = requests.get(activites_url, headers=header, params=param).json()
+
+print(my_dataset[0]["name"])
+print(my_dataset[0]["map"]["summary_polyline"])
